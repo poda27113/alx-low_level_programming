8463223100b09a22dd07c608d6a52d06db57baa0
poda27113 <adeprecio3@gmail.com>
2022-12-28 04:00:55 -0800

    number

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,10 +1,10 @@
-#include "putchar.h"
+#include "main.h"
 
 /**
- *  * print_number - prints an integer.
- *   * @n: input integer.
- *    * Return: no return.
- *     */
+  * print_number - prints an integer.
+   * @n: input integer.
+    * Return: no return.
+     */
 void print_number(int n)
 {
 		unsigned int m, d, count;
