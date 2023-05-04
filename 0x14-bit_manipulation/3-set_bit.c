3e611e55ce80ffd5e750c7ac3a0bd9f7aaf9d866
poda27113 <adeprecio3@gmail.com>
2023-05-04 02:42:59 -0700

    binary

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * Prototype: int set_bit(unsigned long int *n, unsigned int index);
+ * where index is the index, starting from 0 of the bit you want to set
+ * Returns: 1 if it worked, or -1 if an error occurred
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= sizeof(unsigned long int) * 8)
+{
+	return (-1);
+}
+	*n |= (1UL << index);
+	return (1);
+}
---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**Prototype: int clear_bit(unsigned long int *n, unsigned int index);
+ * where index is the index, starting from 0 of the bit you want to set
+ * Returns: 1 if it worked, or -1 if an error occurred
+*/
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= sizeof(unsigned long int) * 8)
+{
+	return (-1);
+}
+	*n &= ~(1UL << index);
+	return 1;
+}
