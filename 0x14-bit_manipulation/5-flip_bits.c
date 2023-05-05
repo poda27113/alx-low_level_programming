9b3d740bc16dfa50fe446b8f26f1e4c3dbe67832
poda27113 <adeprecio3@gmail.com>
2023-05-05 06:24:24 -0700

    rec

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,21 +1,23 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- *
- * Prototype: unsigned int flip_bits(unsigned long int n, unsigned long int m);
- * You are not allowed to use the % or / operators
- * return:o
+ *  * flip_bits - returns the number of bits you would
+ *   * need to flip to get from one number to another
+ *    * @n: number one.
+ *     * @m: number two.
+ *      *
+ *       * Return: number of bits.
  */
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned long int xor = n ^ m;
-	unsigned int count = 0;
+	unsigned int nbits;
 
-	while (xor)
+	for (nbits = 0; n || m; n >>= 1, m >>= 1)
 {
-	count += xor & 1;
-	xor >>= 1;
+	if ((n & 1) != (m & 1))
+	nbits++;
 }
-	return count;
+
+	return (nbits);
 }
