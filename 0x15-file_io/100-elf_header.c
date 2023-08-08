416f1333d09f093e130152567b625b1a53d51385
poda27113 <adeprecio3@gmail.com>
2023-08-08 01:50:26 -0700

    new

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -13,42 +13,44 @@
  *     */
 void print_addr(char *ptr)
 {
-		int i;
-			int begin;
-				char sys;
+	int i;
+	int begin;
+	char sys;
 
-					printf("  Entry point address:               0x");
+	printf("  Entry point address:               0x");
 
-						sys = ptr[4] + '0';
-							if (sys == '1')
-									{
-												begin = 26;
-														printf("80");
-																for (i = begin; i >= 22; i--)
-																			{
-																							if (ptr[i] > 0)
-																												printf("%x", ptr[i]);
-																										else if (ptr[i] < 0)
-																															printf("%x", 256 + ptr[i]);
-																												}
-																		if (ptr[7] == 6)
-																						printf("00");
-																			}
+	sys = ptr[4] + '0';
+	if (sys == '1')
+{
+	begin = 26;
+	printf("80");
+	for (i = begin; i >= 22; i--)
+{
+	if (ptr[i] > 0)
+		printf("%x", ptr[i]);
 
-								if (sys == '2')
-										{
-													begin = 26;
-															for (i = begin; i > 23; i--)
-																		{
-																						if (ptr[i] >= 0)
-																											printf("%02x", ptr[i]);
+	else if (ptr[i] < 0)
+		printf("%x", 256 + ptr[i]);
+}
+	if (ptr[7] == 6)
+	printf("00");
+}
 
-																									else if (ptr[i] < 0)
-																														printf("%02x", 256 + ptr[i]);
+	if (sys == '2')
+{
+	begin = 26;
+	for (i = begin; i > 23; i--)
+{
+	if (ptr[i] >= 0)
 
-																											}
-																}
-									printf("\n");
+	printf("%02x", ptr[i]);
+
+	else if (ptr[i] < 0)
+
+	printf("%02x", 256 + ptr[i]);
+}
+}
+	printf("\n");
 }
 
 /**
@@ -58,33 +60,35 @@ void print_addr(char *ptr)
  *     */
 void print_type(char *ptr)
 {
-		char type = ptr[16];
+	char type = ptr[16];
 
-			if (ptr[5] == 1)
-						type = ptr[16];
-				else
-							type = ptr[17];
+	if (ptr[5] == 1)
+		type = ptr[16];
+	else
+		type = ptr[17];
 
-					printf("  Type:                              ");
-						if (type == 0)
-									printf("NONE (No file type)\n");
-							else if (type == 1)
-										printf("REL (Relocatable file)\n");
-								else if (type == 2)
-											printf("EXEC (Executable file)\n");
-									else if (type == 3)
-												printf("DYN (Shared object file)\n");
-										else if (type == 4)
-													printf("CORE (Core file)\n");
-											else
-														printf("<unknown: %x>\n", type);
+	printf("  Type:                              ");
+	if (type == 0)
+		printf("NONE (No file type)\n");
+	else if (type == 1)
+		printf("REL (Relocatable file)\n");
+	else if (type == 2)
+		printf("EXEC (Executable file)\n");
+	else if (type == 3)
+		printf("DYN (Shared object file)\n");
+	else if (type == 4)
+		printf("CORE (Core file)\n");
+	else
+		printf("<unknown: %x>\n", type);
 }
+
 
 /**
  *  * print_osabi - prints osabi
  *   * @ptr: magic.
  *    * Return: no return.
  *     */
+
 void print_osabi(char *ptr)
 {
 		char osabi = ptr[7];
